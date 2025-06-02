242f7db712d80a5c2ae70b0a763a0bbf5ea0b1b8
dangphuc282 <dangphuc28022002@gmail.com>
2025-06-02 10:09:01 +0900

    Add files via upload

---
+++ arr/main.js
@@ -0,0 +1,12 @@
+// let numbers = [];
+// for(let i = 1; i <= 10; i++){
+//   numbers.push(Math.floor(Math.random()*100));
+// }
+// document.write(numbers);
+const numbers = [];
+
+for (let i = 0; i < 10; i++) {
+  numbers.push(Math.floor(Math.random() * 100));
+}
+
+console.log(numbers);
